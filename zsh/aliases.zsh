09cd982c084b87e2fa5cccc513f6f42a543b4837
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-09 21:51:37 +0900

    aliasを追加

---
+++ zsh/aliases.zsh
@@ -0,0 +1,30 @@
+# ls
+alias ll='ls -alF'
+
+# cd
+alias d='cd ~/Desktop'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# git
+alias g='git'
+alias ga='git add'
+alias gd='git diff'
+alias gs='git status'
+alias gp='git push'
+alias gb='git branch'
+alias gst='git status'
+alias gco='git checkout'
+alias gf='git fetch'
+alias gc='git commit'
+
+# bundler
+alias b='bundle'
+alias bx='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
+
+# global alias
+alias -g C='| pbcopy' # copy
+alias -g G='| grep --color=auto' # 鉄板
